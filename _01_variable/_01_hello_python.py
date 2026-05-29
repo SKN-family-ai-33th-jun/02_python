fe93e04d1a28596f39222b3125ca031b16220fa0
june229 <mswkdh3790@gmail.com>
2026-05-29 16:18:38 +0900

    20260529_변수

---
+++ _01_variable/_01_hello_python.py
@@ -0,0 +1,19 @@
+# 주석(Comment)
+
+# 한 줄 주석
+# ㄷㄹㄴ이ㅓ리?
+
+
+print("Hello Python")
+
+print("Hello World") #문야열
+print('안녕 파이썬')
+print(123) #정수
+print(123.456) #실수
+
+## 여러 줄 문자열 작성
+print("""
+파이썬
+테스트
+중입니다~""")
+
